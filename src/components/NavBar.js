8fb4fb474a6b35cf537fc42135bba95799134bb1
Bravin Murambi <bravvjr@gmail.com>
2024-11-10 15:16:01 +0300

    Adds assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,11 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const displayLinks = links.map((link, index) => {
+    return (<a href={index}>{link}</a>)
+  });
+
+  return <nav>{displayLinks}</nav>;
 }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,13 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+      {projects.map((project) => (
+          <ProjectItem 
+            key={project.id}  // Use the project’s id as the key
+            {...project}      // Spread the project object to pass each property as a prop
+                />
+        ))}      </div>
     </div>
   );
 }
